89396c94f6be1ff96a7c3e4341f527a425705072
= <=>
2023-06-19 10:24:35 +0200

    Jukebox - Song (added simple seeder)

--- Jukebox-app/database/seeders/DatabaseSeeder.php
+++ Jukebox-app/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
   public function run(): void
   {
     $this->call(GenreSeeder::class);
+    $this->call(SongSeeder::class);
     // \App\Models\User::factory(10)->create();
 
     // \App\Models\User::factory()->create([
---
+++ Jukebox-app/database/seeders/SongSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Song;
+
+class SongSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    $songs = [
+      // Hit single real
+      ['name' => 'Joyous'],
+      ['name' => 'Darnell Wet Fart'],
+      // Project MSG Rebirth
+      ['name' => 'Judgement'],
+      ['name' => '25M'],
+      ['name' => 'Chompin\' Beats'],
+      ['name' => 'Can\'t Beat Quickman'],
+      ['name' => 'Blood Of Dracula'],
+      ['name' => 'Overworld'],
+    ];
+    Genre::insert($songs);
+  }
+}
