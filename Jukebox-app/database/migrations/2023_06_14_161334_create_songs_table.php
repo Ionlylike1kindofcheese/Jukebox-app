f9f06782cb1ba3ce22211f89e64621e2bbb19a48
= <=>
2023-06-19 15:18:59 +0200

    Jukebox - Song (Adjusted seeder and migration)

--- Jukebox-app/database/migrations/2023_06_14_161334_create_songs_table.php
+++ Jukebox-app/database/migrations/2023_06_14_161334_create_songs_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('songs', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('author');
+            $table->string('album');
+            $table->dateTime('release');
+            $table->unsignedInteger('duration');
             $table->timestamps();
         });
     }
--- Jukebox-app/database/seeders/SongSeeder.php
+++ Jukebox-app/database/seeders/SongSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Song;
+use Carbon\Carbon;
 
 class SongSeeder extends Seeder
 {
@@ -14,16 +15,18 @@ class SongSeeder extends Seeder
   public function run(): void
   {
     $songs = [
-      // Hit single real
-      ['name' => 'Joyous'],
-      ['name' => 'Darnell Wet Fart'],
-      // Project MSG Rebirth
-      ['name' => 'Judgement'],
-      ['name' => '25M'],
-      ['name' => 'Chompin\' Beats'],
-      ['name' => 'Can\'t Beat Quickman'],
-      ['name' => 'Blood Of Dracula'],
-      ['name' => 'Overworld'],
+      ['name' => 'Joyous', 'author' => 'churgney gurgney', 'album' => 'Hit Single Real', 'release' => Carbon::createFromFormat('d-m-Y', '03-06-2023'), 'duration' => 306],
+      ['name' => 'Bold Or Brash (Darnell Wet Fart)', 'author' => 'churgney gurgney', 'album' => 'Hit Single Real', 'release' => Carbon::createFromFormat('d-m-Y', '03-06-2023'), 'duration' => 231],
+
+      ['name' => 'Judgement', 'author' => 'ZAPZONZAL', 'album' => 'Project MSG Rebirth', 'release' => Carbon::createFromFormat('d-m-Y', '13-03-2023'), 'duration' => 183],
+      ['name' => '25M', 'author' => 'ZAPZONZAL', 'album' => 'Project MSG Rebirth', 'release' => Carbon::createFromFormat('d-m-Y', '13-03-2023'), 'duration' => 225],
+      ['name' => 'Chompin\' Beats', 'author' => 'ZAPZONZAL', 'album' => 'Project MSG Rebirth', 'release' => Carbon::createFromFormat('d-m-Y', '13-03-2023'), 'duration' => 159],
+      ['name' => 'Can\'t Beat Quickman', 'author' => 'ZAPZONZAL', 'album' => 'Project MSG Rebirth', 'release' => Carbon::createFromFormat('d-m-Y', '13-03-2023'), 'duration' => 222],
+      ['name' => 'Blood Of Dracula', 'author' => 'ZAPZONZAL', 'album' => 'Project MSG Rebirth', 'release' => Carbon::createFromFormat('d-m-Y', '13-03-2023'), 'duration' => 273],
+      ['name' => 'Overworld', 'author' => 'ZAPZONZAL', 'album' => 'Project MSG Rebirth', 'release' => Carbon::createFromFormat('d-m-Y', '13-03-2023'), 'duration' => 127],
+
+      ['name' => 'Lose Control', 'author' => 'Hedley', 'album' => 'Hello', 'release' => Carbon::createFromFormat('d-m-Y', '17-06-2016'), 'duration' => 227],
+      ['name' => 'Honest', 'author' => 'Thousand Foot Krutch', 'album' => 'Exhale', 'release' => Carbon::createFromFormat('d-m-Y', '06-11-2015'), 'duration' => 251],
     ];
     Song::insert($songs);
   }
