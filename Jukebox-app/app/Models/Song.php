dc76c1a33c6324c91ad5b9fa5cde657131db9170
= <=>
2023-06-19 13:03:49 +0200

    Jukebox - Playlist & Song (Added many-to-many relation)

--- Jukebox-app/app/Models/Song.php
+++ Jukebox-app/app/Models/Song.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Playlist;
 
 class Song extends Model
 {
   protected $fillable = ["name"];
   use HasFactory;
+
+  public function playlists() {
+    return $this->belongsToMany(Playlist::class);
+  }
 }
