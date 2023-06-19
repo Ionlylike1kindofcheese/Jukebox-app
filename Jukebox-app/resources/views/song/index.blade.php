277ac3f6aabe6e1aa6f73481454960f6e1e14c84
= <=>
2023-06-19 13:05:20 +0200

    Jukebox - Song (added playlist relation display)

--- Jukebox-app/resources/views/song/index.blade.php
+++ Jukebox-app/resources/views/song/index.blade.php
@@ -9,7 +9,15 @@
   <h1>Dit is een totaaloverzicht van alle songs</h1>
   <ul>
   @foreach($songs as $song)
-    <li><a href="{{route('song.destroy', ['song' => $song->id])}}">X</a> {{$song->name}}</li>
+    <li>
+      <a href="{{route('song.destroy', ['song' => $song->id])}}">X</a>
+      {{$song->name}} 
+      @foreach($song->playlists as $playlist) 
+        @if($playlist)
+        / Playlist: {{$playlist->name}}
+        @endif 
+      @endforeach
+    </li>
   @endforeach
   </ul>
   <a href="{{route('song.create')}}">Song Toevoegen</a>
