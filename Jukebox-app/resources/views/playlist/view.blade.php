08772d5a2a1aefeb7ab5c43d6f6ca5e810473680
= <=>
2023-06-29 16:48:40 +0200

    Jukebox - Playlist (Added a total playlist duration calculation)

--- Jukebox-app/resources/views/playlist/view.blade.php
+++ Jukebox-app/resources/views/playlist/view.blade.php
@@ -3,11 +3,18 @@
 @section('mainContent')
   <h1>{{$playlist->name}}</h1>
   <ul>
+  @php
+    $totalDuration = 0;
+  @endphp
   @foreach($songs as $song)
     <li>
       {{$song->name}}
+      @php
+        $totalDuration += $song->duration;
+      @endphp
     </li>
   @endforeach
   </ul>
+  <p>Total duration: {{ gmdate('H:i:s', $totalDuration) }}</p>
   <a href="{{route('playlist.index')}}">Return to overview</a>
 @endsection
