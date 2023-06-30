517898b6973ed84fd59ab43b13d3c590df71f0fc
= <=>
2023-06-30 12:53:09 +0200

    Jukebox - Genre & Song (Filtering songs can now be done from Genre too)

--- Jukebox-app/resources/views/genre/index.blade.php
+++ Jukebox-app/resources/views/genre/index.blade.php
@@ -2,10 +2,11 @@
 
 @section('mainContent')
   <h1>Totaloverview Genres</h1>
+  <p>Selecting a genre will filter songs totaloverview</p>
   <ul>
   @foreach($genres as $genre)
     <li>
-      {{$genre->name}} 
+      <a href="{{route('song.index', ['genre' => $genre->id])}}">{{$genre->name}}</a> 
       @if(auth()->user()?->email == "admin@gmail.com")        
         <a href="{{route('genre.edit', ['genre' => $genre->id])}}">Edit</a> 
       @endif
